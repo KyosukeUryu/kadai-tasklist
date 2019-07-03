187410092509a3d915019724cc13fb3d80886945
kyosuke.uryu <kyo.uryu33@gmail.com>
2019-07-03 13:30:19 +0000

    user_tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,33 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:show, :edit, :create]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task_user, only: [:show, :edit, :update, :destroy]
+  before_action :check_user, only: [:show, :edit, :update, :destroy]
+  #before_action :user_control, only: [:show, :new, :create, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :update, :destroy]
   def index
-    @tasks = Task.all
+    if logged_in?
+      # @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      @tasks = Task.where(user: current_user).order(id: :desc).page(params[:page])
+    end
   end
 
   def show
   end
 
   def new
+    # @task = Task.new
+    # @task = current_user.tasks.build
     @task = Task.new
+    @task.user = current_user
   end
 
   def create
+    # params[:task][:content]
+    # params[:task][:status]
+    # @task = current_user.tasks.build(task_params)
     @task = Task.new(task_params)
+    @task.user = current_user
+    # @task.user_id = current_user.id
 
     if @task.save
       flash[:success] = '新規タスクが作成されました'
@@ -47,11 +61,27 @@ class TasksController < ApplicationController
   
   private
   
-  def set_task
-    @task = Task.find(params[:id])
+  def set_task_user
+    @task = current_user.tasks.find_by(id: params[:id])
+  end
+  
+  def check_user
+    redirect_to root_url if @task.blank?
   end
   
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  #def user_control
+    #@task.user = current_user
+  #end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
 end
